8759347a38077f83847394a4931c511b029f0bdb
chealin <clll1004@gomcorp.com>
2019-04-18 10:22:53 +0900

    주석 수정

--- src/ui_js/ui.js
+++ src/ui_js/ui.js
@@ -19,11 +19,11 @@ function getInternetExplorerVersion() {
   }
   else {
     var ua = navigator.userAgent;
-    var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})"); // IE브라우저 9버전
+    var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
     if (re.exec(ua) != null)
       rv = parseFloat(RegExp.$1);
   }
-  if(rv < 8){
+  if(rv < 8){ // IE브라우저 8버전 이하 시 ie_old 추가
     rv = 8;
     $("html").addClass("ie_old");
   }
