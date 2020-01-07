df989db1596c9df012ae3a01fa565e753a7e92d2
chealin <clll1004@gomcorp.com>
2020-01-07 10:53:21 +0900

    gulp 4.0.0 업데이트

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,12 @@
  * Created by GRE511 on 2019-04-16.
  */
 'use strict';
-var gulp = require('gulp'),
-  browserSync = require('browser-sync').create(),
-  html = require('gulp-html-tag-include'),
-  sass = require('gulp-sass'),
-  autoprefixer = require('gulp-autoprefixer'),
-  sourcemaps = require('gulp-sourcemaps'),
-  uglify = require('gulp-uglify');
+const gulp = require('gulp');
+const browsersync = require('browser-sync').create();
+const html = require('gulp-html-tag-include');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
 
 var paths = {
   port:'9001',
@@ -36,81 +35,69 @@ var paths = {
   }
 };
 
-//browser-sync
-gulp.task('browserSync', function () {
-  console.log("start sync...");
-  return browserSync.init({
-    port:paths.port,
-    server:{
-      baseDir: paths.html.output
-    },
-    startPath:'./html/index.html' //index.html 일반 프로젝트에서는 index.html 로 변경
-  })
-});
+function browserSync(done) {
+    console.log("start sync...");
+    browsersync.init({
+        port:paths.port,
+        server:{
+            baseDir: paths.html.output
+        },
+        startPath:'./html/index.html' //index.html 일반 프로젝트에서는 index.html 로 변경
+    });
+    done();
+}
 
-//gulp-html-tag-include
-gulp.task('html-include', function() {
-  return gulp.src([
-    paths.html.input
-  ])
-    .pipe(html())
-    .pipe(gulp.dest(paths.html.output))
-    .pipe(browserSync.reload({
-      stream:true
-    }))
-});
+function htmlComp() {
+    return gulp.src(paths.html.input)
+        .pipe(html())
+        .pipe(gulp.dest(paths.html.output))
+        .pipe(browsersync.reload({stream:true}));
+}
 
-//scss
-gulp.task('sass', function () {
-  gulp.src(paths.style.input)
-    .pipe(sourcemaps.init()) //소스맵 초기화 및 생성
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(sass({outputStyle:'expanded'})) //nested, expanded, compact, compressed
-    .pipe(sourcemaps.write('.')) //소스맵 작성
-    .pipe(gulp.dest(paths.style.output))
-    .pipe(browserSync.reload({
-      stream:true
-    }));
-  //압축버전 - 이버전은 소스맵이 안맞아서 배포용으로 따로 뺌
-  gulp.src(paths.style.input)
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions','ie 8', 'ie 9']
-    }))
-    .pipe(sass({outputStyle:'compressed'})) //nested, expanded, compact, compressed
-    .pipe(gulp.dest(paths.style.output+'/min'));
-});
+function sassComp(done) {
+    gulp.src(paths.style.input, {sourcemaps: true})
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(sass({outputStyle:'expanded'})) //nested, expanded, compact, compressed
+        .pipe(gulp.dest('./dist/css', { sourcemaps: true }))
+        .pipe(browsersync.reload({stream:true}));
+    // 압축버전 - 이버전은 소스맵이 안맞아서 배포용으로 따로 뺌
+    gulp.src(paths.style.input, {sourcemaps: true})
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions','ie 8', 'ie 9']
+        }))
+        .pipe(sass({outputStyle:'compressed'})) //nested, expanded, compact, compressed
+        .pipe(gulp.dest(paths.style.output+'/min', { sourcemaps: true }));
+    done();
+}
 
-gulp.task('js',function(){
-  gulp.src(paths.js.input)
-    .pipe(gulp.dest(paths.js.output))
-    .pipe(uglify())
-    .pipe(gulp.dest(paths.js.output+'/min'))
-    .pipe(browserSync.reload({
-      stream:true
-    }));
-});
+function js() {
+    return gulp.src(paths.js.input, {sourcemaps: true})
+        .pipe(gulp.dest(paths.js.output))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.js.output+'/min', {sourcemaps: true}))
+        .pipe(browsersync.reload({stream:true}));
+}
 
-gulp.task('img',function(){
-	gulp.src(paths.img.input)
-		.pipe(gulp.dest(paths.img.output))
-		.pipe(browserSync.reload({
-			stream:true
-		}));
-});
+function img() {
+  return gulp.src(paths.img.input)
+      .pipe(gulp.dest(paths.img.output))
+      .pipe(browsersync.reload({stream:true}));
+}
 
-gulp.task('font',function(){
-	gulp.src(paths.font.input)
-		.pipe(gulp.dest(paths.font.output))
-		.pipe(browserSync.reload({
-			stream:true
-		}));
-});
+function font() {
+  return gulp.src(paths.font.input)
+      .pipe(gulp.dest(paths.font.output))
+      .pipe(browsersync.reload({stream:true}));
+}
 
-gulp.task('watch', ['html-include','sass','js','img','font'], function() {
-  gulp.watch(paths.style.input, ['sass']);
-  gulp.watch(paths.html.input, ['html-include']);
-  gulp.watch(paths.js.input, ['js']);
-});
+function watchFiles() {
+    gulp.watch(paths.html.input, htmlComp);
+    gulp.watch(paths.img.input, img);
+    gulp.watch(paths.style.input, sassComp);
+    gulp.watch(paths.js.input, js);
+    gulp.watch(paths.font.input, font);
+}
 
-gulp.task('default',['browserSync','watch']);
+// export tasks
+exports.default = gulp.series(gulp.parallel(htmlComp, sassComp, img, js, font), browserSync, watchFiles);
