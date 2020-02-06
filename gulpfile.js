d2a42a261ae20f85d8da5e05ead28908a1c35f0c
chealin <clll1004@gomcorp.com>
2020-02-06 09:25:43 +0900

    gulp sourcemap 세팅 수정

--- gulpfile.js
+++ gulpfile.js
@@ -8,93 +8,95 @@ const html = require('gulp-html-tag-include');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
 
 const paths = {
-    port:'9001',
-    input:'./src/**/*',
-    output:'./dist/',
-    html: {
-        input: './src/**/*.html',
-        output: './dist/'
-    },
-    style:{
-        input:'./src/scss/*.scss',
-        output:'./dist/css'
-    },
-    img:{
-        input:'./src/img/**/*',
-        output:'./dist/img'
-    },
-    font:{
-        input:'./src/font/**/*',
-        output:'./dist/font'
-    },
-    js: {
-        input : './src/ui_js/*.js',
-        output : './dist/ui_js/'
-    }
+  port: '9001',
+  input: './src/**/*',
+  output: './dist/',
+  html: {
+    input: './src/**/*.html',
+    output: './dist/'
+  },
+  style: {
+    input: './src/scss/*.scss',
+    output: './dist/css'
+  },
+  img: {
+    input: './src/img/**/*',
+    output: './dist/img'
+  },
+  font: {
+    input: './src/font/**/*',
+    output: './dist/font'
+  },
+  js: {
+    input: './src/ui_js/*.js',
+    output: './dist/ui_js/'
+  }
 };
 
 function browserSync(done) {
-    console.log("start sync...");
-    browsersync.init({
-        port:paths.port,
-        server:{
-            baseDir: paths.html.output
-        },
-        startPath:'./html/index.html' //index.html 일반 프로젝트에서는 index.html 로 변경
-    });
-    done();
+  console.log("start sync...");
+  browsersync.init({
+    port: paths.port,
+    server: {
+      baseDir: paths.html.output
+    },
+    startPath: './html/index.html' //index.html 일반 프로젝트에서는 index.html 로 변경
+  });
+  done();
 }
 
 function htmlComp() {
-    return gulp.src(paths.html.input)
-        .pipe(html())
-        .pipe(gulp.dest(paths.html.output))
-        .pipe(browsersync.reload({stream:true}));
+  return gulp.src(paths.html.input)
+    .pipe(html())
+    .pipe(gulp.dest(paths.html.output))
+    .pipe(browsersync.reload({stream: true}));
 }
 
 function sassComp(done) {
-    gulp.src(paths.style.input, {sourcemaps: true})
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(sass({outputStyle:'expanded'})) //nested, expanded, compact, compressed
-        .pipe(gulp.dest(paths.style.output, { sourcemaps: true }))
-        .pipe(browsersync.reload({stream:true}));
-    // 압축버전 - 이버전은 소스맵이 안맞아서 배포용으로 따로 뺌
-    gulp.src(paths.style.input, {sourcemaps: true})
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(sass({outputStyle:'compressed'})) //nested, expanded, compact, compressed
-        .pipe(gulp.dest(paths.style.output+'/min', { sourcemaps: true }));
-    done();
+  gulp.src(paths.style.input, {sourcemaps: true})
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'expanded'})) //nested, expanded, compact, compressed
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.style.output))
+    .pipe(browsersync.reload({stream: true}));
+  // 압축버전 - 이버전은 소스맵이 안맞아서 배포용으로 따로 뺌
+  gulp.src(paths.style.input, {sourcemaps: true})
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sass({outputStyle: 'compressed'})) //nested, expanded, compact, compressed
+    .pipe(gulp.dest(paths.style.output + '/min'));
+  done();
 }
 
 function js() {
-    return gulp.src(paths.js.input, {sourcemaps: true})
-        .pipe(gulp.dest(paths.js.output))
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.js.output+'/min', {sourcemaps: true}))
-        .pipe(browsersync.reload({stream:true}));
+  return gulp.src(paths.js.input)
+    .pipe(gulp.dest(paths.js.output))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.js.output + '/min'))
+    .pipe(browsersync.reload({stream: true}));
 }
 
 function img() {
   return gulp.src(paths.img.input)
-      .pipe(gulp.dest(paths.img.output))
-      .pipe(browsersync.reload({stream:true}));
+    .pipe(gulp.dest(paths.img.output))
+    .pipe(browsersync.reload({stream: true}));
 }
 
 function font() {
   return gulp.src(paths.font.input)
-      .pipe(gulp.dest(paths.font.output))
-      .pipe(browsersync.reload({stream:true}));
+    .pipe(gulp.dest(paths.font.output))
+    .pipe(browsersync.reload({stream: true}));
 }
 
 function watchFiles() {
-    gulp.watch(paths.html.input, htmlComp);
-    gulp.watch(paths.img.input, img);
-    gulp.watch(paths.style.input, sassComp);
-    gulp.watch(paths.js.input, js);
-    gulp.watch(paths.font.input, font);
+  gulp.watch(paths.html.input, htmlComp);
+  gulp.watch(paths.img.input, img);
+  gulp.watch(paths.style.input, sassComp);
+  gulp.watch(paths.js.input, js);
+  gulp.watch(paths.font.input, font);
 }
 
 // export tasks
